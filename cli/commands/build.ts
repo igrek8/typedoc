637efae93a6e6817787e0cdb9e160b30ee705d12
Igor Korchagin <igor.korchagin8@gmail.com>
2023-02-26 17:39:41 +0100

    refactor: move executable to bin

--- cli/commands/build.ts
+++ cli/commands/build.ts
@@ -14,6 +14,7 @@ export default program
     const cwd = process.cwd();
     const options: ExecSyncOptionsWithBufferEncoding = { cwd, env: process.env, stdio: 'inherit' };
     const typedocOptions = path.join(__dirname, '..', '..', 'typedoc.js');
-    execSync(`typedoc --out ${out} --options ${typedocOptions}`, options);
+    const typedoc = path.resolve(require.resolve('typedoc/package.json'), '..', 'bin', 'typedoc');
+    execSync(`${typedoc} --out ${out} --options ${typedocOptions}`, options);
     writeFileSync(path.join(cwd, out, 'index.html'), readFileSync(path.join(cwd, 'public', 'index.html')));
   });
--- cli/commands/publish.ts
+++ cli/commands/publish.ts
@@ -1,5 +1,6 @@
 import { execSync, ExecSyncOptionsWithBufferEncoding } from 'child_process';
 import { program } from 'commander';
+import * as path from 'path';
 
 export interface PublishDocsCommandOptions {
   out: string;
@@ -11,5 +12,6 @@ export default program
   .action(({ out }: PublishDocsCommandOptions) => {
     const cwd = process.cwd();
     const options: ExecSyncOptionsWithBufferEncoding = { cwd, env: process.env, stdio: 'inherit' };
-    execSync(`gh-pages --dist ${out}`, options);
+    const pages = path.resolve(require.resolve('gh-pages/package.json'), '..', 'bin', 'gh-pages.js');
+    execSync(`${pages} --dist ${out}`, options);
   });
